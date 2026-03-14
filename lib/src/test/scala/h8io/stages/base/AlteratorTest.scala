4613c7d9e9eecf6965190c2b30260d3e6a1a4463
Eshu <tjano.xibalba@gmail.com>
2026-03-14 20:35:57 +0900

    Double exception tests

--- lib/src/test/scala/h8io/stages/base/AlteratorTest.scala
+++ lib/src/test/scala/h8io/stages/base/AlteratorTest.scala
@@ -19,20 +19,6 @@ class AlteratorTest extends AnyFlatSpec with Matchers with MockFactory {
     noException should be thrownBy alterator.dispose()
   }
 
-  it should "call beforeDispose and afterDispose even if alterand's dispose throws" in {
-    val alterand = mock[Stage[Any, Nothing, Nothing]]("alterand")
-    val alterator = mock[AlteratorTest.TestAlterator]("alterator")
-    val context = new Object
-    val exception = new Exception("alterand.dispose failed")
-    inSequence {
-      (alterator.beforeDispose _).expects().returns(context)
-      (() => alterator.alterand).expects().returns(alterand)
-      (alterand.dispose _).expects().throws(exception)
-      (alterator.afterDispose _).expects(context)
-    }
-    the[Exception] thrownBy alterator.dispose() should be theSameInstanceAs exception
-  }
-
   it should "call alterand's dispose even if beforeDispose throws" in {
     val alterand = mock[Stage[Any, Nothing, Nothing]]("alterand")
     val alterator = mock[AlteratorTest.TestAlterator]("alterator")
@@ -41,6 +27,20 @@ class AlteratorTest extends AnyFlatSpec with Matchers with MockFactory {
       (alterator.beforeDispose _).expects().throws(exception)
       (() => alterator.alterand).expects().returns(alterand)
       (alterand.dispose _).expects().returns(())
+    }
+    the[Exception] thrownBy alterator.dispose() should be theSameInstanceAs exception
+  }
+
+  it should "call beforeDispose and afterDispose even if alterand's dispose throws" in {
+    val alterand = mock[Stage[Any, Nothing, Nothing]]("alterand")
+    val alterator = mock[AlteratorTest.TestAlterator]("alterator")
+    val context = new Object
+    val exception = new Exception("alterand's dispose failed")
+    inSequence {
+      (alterator.beforeDispose _).expects().returns(context)
+      (() => alterator.alterand).expects().returns(alterand)
+      (alterand.dispose _).expects().throws(exception)
+      (alterator.afterDispose _).expects(context)
     }
     the[Exception] thrownBy alterator.dispose() should be theSameInstanceAs exception
   }
@@ -57,6 +57,38 @@ class AlteratorTest extends AnyFlatSpec with Matchers with MockFactory {
       (alterator.afterDispose _).expects(context).throws(exception)
     }
     the[Exception] thrownBy alterator.dispose() should be theSameInstanceAs exception
+  }
+
+  it should "suppress alterand's dispose exception if beforeDispose throws" in {
+    val alterand = mock[Stage[Any, Nothing, Nothing]]("alterand")
+    val alterator = mock[AlteratorTest.TestAlterator]("alterator")
+    val beforeDisposeException = new Exception("beforeDispose failed")
+    val alterandDisposeException = new Exception("alterand's dispose failed")
+    inSequence {
+      (alterator.beforeDispose _).expects().throws(beforeDisposeException)
+      (() => alterator.alterand).expects().returns(alterand)
+      (alterand.dispose _).expects().throws(alterandDisposeException)
+    }
+    val exception = the[Exception] thrownBy alterator.dispose()
+    exception should be theSameInstanceAs beforeDisposeException
+    beforeDisposeException.getSuppressed should contain(alterandDisposeException)
+  }
+
+  it should "suppress afterDispose exception if alterand's dispose throws" in {
+    val alterand = mock[Stage[Any, Nothing, Nothing]]("alterand")
+    val alterator = mock[AlteratorTest.TestAlterator]("alterator")
+    val context = new Object
+    val alterandDisposeException = new Exception("alterand's dispose failed")
+    val afterDisposeException = new Exception("afterDispose failed")
+    inSequence {
+      (alterator.beforeDispose _).expects().returns(context)
+      (() => alterator.alterand).expects().returns(alterand)
+      (alterand.dispose _).expects().throws(alterandDisposeException)
+      (alterator.afterDispose _).expects(context).throws(afterDisposeException)
+    }
+    val exception = the[Exception] thrownBy alterator.dispose()
+    exception should be theSameInstanceAs alterandDisposeException
+    alterandDisposeException.getSuppressed should contain(afterDisposeException)
   }
 }
 
