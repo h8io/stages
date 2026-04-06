ac6c92f21e700eb88aa0ef6d778c2be244c27361
Eshu <tjano.xibalba@gmail.com>
2026-04-06 23:19:41 +0900

    Better naming

--- lib/src/main/scala/h8io/stages/base/ConstEvolution.scala
+++ lib/src/main/scala/h8io/stages/base/ConstEvolution.scala
@@ -2,7 +2,7 @@ package h8io.stages.base
 
 import h8io.stages.{Evolution, Stage}
 
-final case class StageEvolution[-I, +O, +E](stage: Stage[I, O, E]) extends Evolution[I, O, E] {
+final case class ConstEvolution[-I, +O, +E](stage: Stage[I, O, E]) extends Evolution[I, O, E] {
   override def onSuccess(): Stage[I, O, E] = stage
   override def onComplete(): Stage[I, O, E] = stage
   override def onError(): Stage[I, O, E] = stage
